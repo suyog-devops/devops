68a09e3874d055cf0857c49122460fe391c23e6e
suyog-devops <suyog.gandhi.devops@gmail.com>
2025-12-15 11:29:58 +0530

    s3 bucket code

---
+++ Terraform/terraform-static-webiste-S3/variables.tf
@@ -0,0 +1,5 @@
+variable "s3-bucket-name" {
+    description = "Name of the S3 bucket to host the static website"
+    type        = string
+    default = "static-website-bucket-1234567890"
+}
---
+++ Terraform/terraform-static-webiste-S3/website/script.js
@@ -0,0 +1,3 @@
+function showMessage() {
+  alert("Hello from S3 + CloudFront!");
+}
